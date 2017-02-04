e74642cde8be3de12f0d29fb36dab0294707175c
ruxrux <ruipereira@ruipereira-macbookpro2.roam.corp.google.com>
2017-02-03 22:02:16 -0500

    clean up and add files

---
+++ Vuforia/Vuforia_example/Assets/_Scripts/HItManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HItManager : MonoBehaviour {
+
+	private RaycastHit hit;
+
+	// Update is called once per frame
+	void Update () {
+		Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+		if(Physics.Raycast(ray, out hit) && Input.GetMouseButton(0) && hit.transform.tag == "fake"){
+			Debug.Log ("HIT!!!!");
+			hit.transform.gameObject.SendMessage ("OnPoke");
+		}
+	}
+}
+
---
+++ Vuforia/Vuforia_example/Assets/_Scripts/LookAtMe.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAtMe : MonoBehaviour {
+
+	// transform from the object we want to be looking at
+	public Transform target;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		// use the target coordinates to rotate this object
+		this.transform.LookAt(target);
+
+	}
+}
