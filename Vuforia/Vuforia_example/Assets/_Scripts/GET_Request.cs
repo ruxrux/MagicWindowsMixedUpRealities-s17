47284c559b1877b80ebd3a0d2861ea9be08a80a2
ruxrux <ruipereira@ruipereira-macbookpro2.roam.corp.google.com>
2017-02-10 14:43:27 -0500

    added GET request code

---
+++ Vuforia/Vuforia_example/Assets/_Scripts/GET_Request.cs
@@ -0,0 +1,78 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+/* * * * * * * * * * * 
+ * don't forget to add simple JSON!
+ * get the c# code from here:
+ * http://wiki.unity3d.com/index.php/SimpleJSON#CSharp
+ * paste it in a c# file, make sure you save the file with the name of the class (SimpleJSON) 
+ * import the file to /Assets/PlugIns/SimpleJSON
+*/
+
+using SimpleJSON;
+
+class GET_Request: MonoBehaviour {
+
+	private string weatherType;
+
+	// create public array of game objects for weather icons
+	public GameObject[] weatherShapes;
+	private GameObject todaysWeatherIcon;
+	private bool bIsObject = false;
+
+	void Start() {
+		StartCoroutine(GetText());
+	}
+
+	IEnumerator GetText() {
+
+		// using http://openweathermap.org/api 
+		UnityWebRequest www = UnityWebRequest.Get("http://api.openweathermap.org/data/2.5/weather?q=NewYork&APPID=702367da6775090551215694c7a7fea9");
+		yield return www.Send();
+
+		if(www.isError) {
+			Debug.Log(www.error);
+		}
+		else {
+			// Show results as text
+			//Debug.Log(www.downloadHandler.text);
+
+			// create an object so we can feed the api call JSON result to parse
+			var DATA = JSON.Parse(www.downloadHandler.text);
+			// look for the values and attach it to the vars 
+			string Location = DATA ["name"].Value;
+			weatherType  = DATA ["weather"][0]["main"].Value;
+
+			Debug.Log ("Location :: " + Location);
+			Debug.Log ("Weather :: " + weatherType);
+
+		}
+	}
+
+	void Update(){
+
+		if (!bIsObject) {
+			if (weatherType == "Clear") {
+				// create an instance of our prefab on the scene (making it 'real'!) 
+				// more about prefabs and instantiating : https://docs.unity3d.com/Manual/InstantiatingPrefabs.html
+				todaysWeatherIcon = Instantiate<GameObject> (weatherShapes[0]);
+				bIsObject = true;
+
+			} else if  (weatherType == "Clouds") {
+				todaysWeatherIcon = Instantiate<GameObject> (weatherShapes[1]);
+				// rotate the cloud 
+				todaysWeatherIcon.transform.Rotate (Vector3.forward, 90f);
+				bIsObject = true;
+
+			}
+		}
+
+		// let's give it some life!
+		// make sure the object exists
+		if (todaysWeatherIcon != null) {
+			// animate its position using a sin (sin and cos are your friends!)
+			todaysWeatherIcon.transform.position = new Vector3 (.0f, .1f+(Mathf.Sin (Time.time * 2) * .02f), .0f);
+		}
+	}
+}
---
+++ Vuforia/Vuforia_example/Assets/_Scripts/TestLocationService.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+
+public class TestLocationService : MonoBehaviour
+{
+	public Text info;
+	private string debug;
+
+	IEnumerator Start()
+	{
+		// First, check if user has location service enabled
+		if (!Input.location.isEnabledByUser) {
+			debug = "Loc disabled";
+			//Debug.Log (debug);
+			info.text = debug;
+
+			yield break;
+		}
+		// Start service before querying location
+		Input.location.Start();
+		debug = ":: Loc services requested ::";
+		//Debug.Log (debug);
+		info.text = debug;
+		// Wait until service initializes
+		int maxWait = 20;
+		while (Input.location.status == LocationServiceStatus.Initializing && maxWait > 0)
+		{
+			yield return new WaitForSeconds(1);
+			maxWait--;
+		}
+
+		// Service didn't initialize in 20 seconds
+		if (maxWait < 1)
+		{
+			debug = "Timed out";
+			//Debug.Log (debug);
+			info.text = debug;
+			yield break;
+		}
+
+		// Connection has failed
+		if (Input.location.status == LocationServiceStatus.Failed)
+		{
+			debug = "Unable to determine device location";
+			//Debug.Log (debug);
+			info.text = debug;
+			yield break;
+		}
+		else
+		{
+			// Access granted and location value could be retrieved
+			debug = "Location: " + Input.location.lastData.latitude + " " + Input.location.lastData.longitude + " " + Input.location.lastData.altitude + " " + Input.location.lastData.horizontalAccuracy + " " + Input.location.lastData.timestamp;
+			//Debug.Log (debug);
+			info.text = debug;
+			
+		}
+
+
+		// Stop service if there is no need to query location updates continuously
+		Input.location.Stop();
+	}
+}
