6e1ee0d0cf75597a7a99d4ae631556b4ba9fa791
ruxrux <rux.pgp@gmail.com>
2017-10-12 23:04:45 -0400

    added multiple targets example

---
+++ week06/multipleTargetsManager/Assets/Vuforia/Scripts/DefaultTrackableEventHandler.cs
@@ -0,0 +1,123 @@
+/*==============================================================================
+Copyright (c) 2010-2014 Qualcomm Connected Experiences, Inc.
+All Rights Reserved.
+Confidential and Proprietary - Protected under copyright and other laws.
+==============================================================================*/
+
+using UnityEngine;
+
+namespace Vuforia
+{
+    /// <summary>
+    /// A custom handler that implements the ITrackableEventHandler interface.
+    /// </summary>
+    public class DefaultTrackableEventHandler : MonoBehaviour,
+                                                ITrackableEventHandler
+    {
+        #region PRIVATE_MEMBER_VARIABLES
+ 
+        private TrackableBehaviour mTrackableBehaviour;
+    
+        #endregion // PRIVATE_MEMBER_VARIABLES
+
+
+
+        #region UNTIY_MONOBEHAVIOUR_METHODS
+    
+        void Start()
+        {
+            mTrackableBehaviour = GetComponent<TrackableBehaviour>();
+            if (mTrackableBehaviour)
+            {
+                mTrackableBehaviour.RegisterTrackableEventHandler(this);
+            }
+        }
+
+        #endregion // UNTIY_MONOBEHAVIOUR_METHODS
+
+
+
+        #region PUBLIC_METHODS
+
+        /// <summary>
+        /// Implementation of the ITrackableEventHandler function called when the
+        /// tracking state changes.
+        /// </summary>
+        public void OnTrackableStateChanged(
+                                        TrackableBehaviour.Status previousStatus,
+                                        TrackableBehaviour.Status newStatus)
+        {
+            if (newStatus == TrackableBehaviour.Status.DETECTED ||
+                newStatus == TrackableBehaviour.Status.TRACKED ||
+                newStatus == TrackableBehaviour.Status.EXTENDED_TRACKED)
+            {
+                OnTrackingFound();
+            }
+            else
+            {
+                OnTrackingLost();
+            }
+        }
+
+        #endregion // PUBLIC_METHODS
+
+
+
+        #region PRIVATE_METHODS
+
+
+        private void OnTrackingFound()
+        {
+            Renderer[] rendererComponents = GetComponentsInChildren<Renderer>(true);
+            Collider[] colliderComponents = GetComponentsInChildren<Collider>(true);
+			AudioSource soundPlayer = GetComponentInChildren<AudioSource> ();
+
+            // Enable rendering:
+            foreach (Renderer component in rendererComponents)
+            {
+                component.enabled = true;
+            }
+
+            // Enable colliders:
+            foreach (Collider component in colliderComponents)
+            {
+                component.enabled = true;
+            }
+
+			// Enable audio source and start playback
+			soundPlayer.enabled = true;
+			soundPlayer.Play ();
+
+            Debug.Log("Trackable " + mTrackableBehaviour.TrackableName + " found");
+        }
+
+
+        private void OnTrackingLost()
+        {
+            Renderer[] rendererComponents = GetComponentsInChildren<Renderer>(true);
+            Collider[] colliderComponents = GetComponentsInChildren<Collider>(true);
+			AudioSource soundPlayer = GetComponentInChildren<AudioSource> ();
+
+            // Disable rendering:
+            foreach (Renderer component in rendererComponents)
+            {
+                component.enabled = false;
+            }
+
+            // Disable colliders:
+            foreach (Collider component in colliderComponents)
+            {
+                component.enabled = false;
+            }
+
+
+			// Stop playback and disable audio source 
+			soundPlayer.Stop ();
+			soundPlayer.enabled = false;
+
+            Debug.Log("Trackable " + mTrackableBehaviour.TrackableName + " lost");
+        }
+
+        #endregion // PRIVATE_METHODS
+    }
+}
