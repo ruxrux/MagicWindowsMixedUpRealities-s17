9e104d619af75c5cd172c7b28a9abd9b41aa80e6
ruxrux <rux.pgp@gmail.com>
2017-12-01 22:15:26 -0500

    added tango ar example
    tango, ar, spatial sound, occlusion

---
+++ week12/tango_AR/Assets/_Scripts/RaycastLook.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// https://docs.unity3d.com/ScriptReference/Physics.Raycast.html
+
+public class RaycastLook : MonoBehaviour {
+
+	public Transform origin;		// public var to get the ray's origin  
+	public float rayReach = 10.0f; // the length of the ray 
+	public string fName;
+	// Update is called once per frame
+	void Update () {
+		// var to store the  hit object's info
+		RaycastHit hit;
+		// to ray cast you need :  an origin 		   + direction			   + a var to store the hit object's data 
+		if( Physics.Raycast(origin.transform.position, origin.transform.forward, out hit, rayReach) ){
+
+			// we dont want to count the plane as a hit
+			if (hit.collider.name != "Plane") {
+
+				Debug.Log (hit.collider.name + " > " + hit.distance);
+				//Debug.Log (hit.collider.tag);
+
+				// find the object and call its Animate() function
+				GameObject.Find (hit.collider.name).SendMessage (fName);
+
+				// https://docs.unity3d.com/ScriptReference/GameObject.SendMessage.html
+				// if the  object's function you're calling has parameters < e.g. void Animate(float speed) > 
+				// you have to pass them < e.g. SendMessage("Animate", 1.0f) >
+				// you can also find the object through a tag
+				// GameObject.FindGameObjectWithTag ("tag").SendMessage ("Animate");
+				// or multiple objects if they have the same tag
+				// GameObject.FindGameObjectsWithTag ...
+			}
+		}
+	}
+}
---
+++ week12/tango_AR/Assets/_Scripts/objectUIController.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class objectUIController : MonoBehaviour {
+
+	// public array for our objects
+	public GameObject [] myObject;
+
+	private TangoPointCloud pointCloud;
+
+
+	// Use this for initialization
+	void Start () {
+		// find our Tango point cloud and attach it to our pc object
+		pointCloud = FindObjectOfType<TangoPointCloud>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		// count only first touch - disregard following finger touches
+		if(Input.touchCount == 1){
+
+			// get the touch position
+			Touch t = Input.GetTouch(0);
+
+			// place object on touch position when touch ended
+			if(t.phase == TouchPhase.Ended){
+				PlaceObject (t.position, "FRONTOF");
+			}
+		}
+	}
+
+	void PlaceObject(Vector2 _touchPos, string _mode){
+
+		Camera cam = Camera.main;
+
+		if (_mode == "FRONTOF") {
+			// IN FRONT OF
+
+		// Instantiate our object on the scene right on front of our camera
+		Vector3 frontPOS  = cam.transform.position + cam.transform.forward * .5f;
+			Instantiate (myObject[Random.Range(0,myObject.Length)], frontPOS, Quaternion.identity);
+
+		} else if (_mode == "POINTS") {
+			// FIND POINTS //
+
+			// FindClosestPoint() is a very intensive function as it goes through every single point in the pc - ideally called no more than once per frame
+			int pointIndex = pointCloud.FindClosestPoint (cam, _touchPos, 10);
+
+			if (pointIndex > -1) {
+				// Index is valid
+				Vector3 pos = pointCloud.m_points [pointIndex];
+				// add, Instantiate our object on the scene 
+				Instantiate (myObject [Random.Range (0, myObject.Length)], pos, Quaternion.LookRotation (cam.transform.forward, cam.transform.up));
+			}
+
+		} else if (_mode == "PLANES") {
+		// FIND PLANES //
+
+		// vector3 to store the plance center pos
+		Vector3 planeCenter;
+		// plane primitive to store the found plane
+		Plane plane;
+
+		// look for planes - only continue if a plane has been found
+		if(!pointCloud.FindPlane(cam, _touchPos, out planeCenter, out plane )){
+			Debug.Log ("Can't find plane...");
+			return;
+		}
+
+		// if it has found a plane, now we can use it to place our object
+		// first make sure the plane isnt at a steep angle i.e.
+//		if (Vector3.Angle (plane.normal, Vector3.up) < 30.0f) {
+
+		// find up, right and forward
+		Vector3 up = plane.normal;
+		Vector3 right = Vector3.Cross (plane.normal, cam.transform.forward).normalized;
+		Vector3 forward = Vector3.Cross (right, plane.normal).normalized;
+
+			// add, Instantiate our object on the scene 
+		Instantiate (myObject[Random.Range(0,myObject.Length)], planeCenter, Quaternion.LookRotation (forward, up));
+//		} else {
+//			Debug.Log ("surface is too steep");
+//		}
+
+		}
+	}
+}
