0a607706bcc380c7b15e33d910f6c3f60ebac73b
ruxrux <rux.pgp@gmail.com>
2018-02-22 23:34:26 -0500

    adding arkit example

---
+++ week05/arkit_hit/Assets/_scripts/envLighting.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.iOS;
+
+public class envLighting : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		UnityARSessionNativeInterface.ARFrameUpdatedEvent += UpdatedAmbientIntensity;
+	}
+
+	void UpdatedAmbientIntensity (UnityARCamera camera)
+	{
+		float newLight = camera.lightData.arLightEstimate.ambientIntensity /1000f;
+		RenderSettings.ambientLight = Color.white * newLight;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ week05/arkit_hit/Assets/_scripts/hitTest.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.XR.iOS; // using arkit namespace
+
+public class hitTest : MonoBehaviour {
+
+	// text for debug
+	//public Text debugTxt;
+
+	//add public to link augmentable prefab
+	//create object to put it on
+	public GameObject hitPrefab; // augmentable we will replicate on each hit test
+	GameObject hitObject;			
+	AudioSource sample;
+
+	void Start(){
+		//instantiate prefab 
+		hitObject = Instantiate<GameObject> (hitPrefab);
+		sample = hitObject.GetComponent<AudioSource> ();
+
+		//debugTxt.text = "Starting... ";// + sample.name;
+
+	}
+
+
+	void onHit(ARPoint point){
+
+		// prioritize the results
+		ARHitTestResultType[] resultTypes = {
+			ARHitTestResultType.ARHitTestResultTypeExistingPlaneUsingExtent,
+			ARHitTestResultType.ARHitTestResultTypeExistingPlane,
+			ARHitTestResultType.ARHitTestResultTypeHorizontalPlane,
+			ARHitTestResultType.ARHitTestResultTypeFeaturePoint
+			//ARHitTestResultType.ARHitTestResultTypeVerticalPlane,
+
+		};
+
+		foreach(ARHitTestResultType resultType in resultTypes){
+			List<ARHitTestResult> hitResults = UnityARSessionNativeInterface.GetARSessionNativeInterface ().HitTest (point, resultType);
+
+			if (hitResults.Count > 0) {
+
+				foreach(var hitResult in hitResults){
+					//Debug.Log ("GOT HIT!!!");
+					//debugTxt.text = "got hit!";
+
+					hitObject.transform.position = UnityARMatrixOps.GetPosition (hitResult.worldTransform);
+					hitObject.transform.rotation = UnityARMatrixOps.GetRotation (hitResult.worldTransform);
+					sample.Play ();
+				}
+
+			}
+		}
+
+	}
+
+
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.touchCount > 0) {
+			var touch = Input.GetTouch (0);
+			if (touch.phase == TouchPhase.Began || touch.phase == TouchPhase.Moved) {
+				var screenPosition = Camera.main.ScreenToViewportPoint (touch.position);
+				ARPoint point = new ARPoint {
+					x = screenPosition.x,
+					y = screenPosition.y
+				};
+
+				//debugTxt.text = "got touch";
+				// do a hit test
+				// lets test if we hit a desired surface/feature and add an object
+				onHit (point);
+			}
+		}
+	}
+}
+
+
